3f70f9c30004f0caad96c87ac8867c60f20918d9
Pandu Dwi Putra Nugroho <pandudpn@gmail.com>
2019-12-13 12:52:16 +0700

    make migration and seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name'          => 'Administrator',
-            'username'      => 'admin',
-            'password'      => bcrypt(sha1('adminadmin')),
-            'created_at'    => now(),
-            'updated_at'    => now()
+        DB::table('accounts')->insert([
+            'name'            => "Administrator",
+            'password'        => bcrypt(sha1('admin')),
+            'email'           => 'admin@admin.com'
         ]);
     }
 }
